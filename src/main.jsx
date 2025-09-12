6fac6689bc3d6c3b893a345b65b3829f034619fe
Bouchdak <info@bluelineccs.nl>
2025-09-12 17:13:25 +0200

    Fix login crash: wrap routes with AuthProvider (no guards)

--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,23 @@
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { AuthProvider } from './providers/AuthProvider';
 
 import BluelineChatpilot from './components/BluelineChatpilot';
 import Login from './pages/Login';
 import AuthCallback from './pages/AuthCallback';
 
-import './index.css'; // zorg dat Tailwind geladen blijft
+import './index.css';
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-    <Routes>
-      <Route path="/login" element={<Login />} />
-      <Route path="/auth/callback" element={<AuthCallback />} />
-
-      {/* jouw app blijft vrij toegankelijk */}
-      <Route path="/app" element={<BluelineChatpilot />} />
-
-      {/* default naar /app */}
-      <Route path="*" element={<Navigate to="/app" replace />} />
-    </Routes>
+    <AuthProvider>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/auth/callback" element={<AuthCallback />} />
+        {/* Chatpilot blijft vrij toegankelijk */}
+        <Route path="/app" element={<BluelineChatpilot />} />
+        <Route path="*" element={<Navigate to="/app" replace />} />
+      </Routes>
+    </AuthProvider>
   </BrowserRouter>
 );
