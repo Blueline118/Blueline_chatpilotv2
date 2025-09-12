b7177ba422e3036a482e004962ca3ceb7340be52
Bouchdak <info@bluelineccs.nl>
2025-09-12 16:19:08 +0200

    Restore stable entry files from last green Netlify deploy

--- src/main.jsx
+++ src/main.jsx
@@ -1,35 +1,10 @@
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import { AuthProvider } from './providers/AuthProvider';
-
-import Protected from './components/Protected';
-import AppHome from './pages/AppHome';          // bestaat al en werkt
-import Login from './pages/Login';
-import AuthCallback from './pages/AuthCallback';
-
-// Optioneel: als je al een aparte Chatpagina gemaakt hebt:
-// import CxChat from './pages/CxChat';
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <AuthProvider>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/auth/callback" element={<AuthCallback />} />
-
-        {/* Simpel en stabiel: gebruik je bestaande AppHome als /app */}
-        <Route
-          path="/app"
-          element={
-            <Protected>
-              <AppHome />
-            </Protected>
-          }
-        />
-
-        {/* Default redirect */}
-        <Route path="*" element={<Navigate to="/app" replace />} />
-      </Routes>
-    </AuthProvider>
-  </BrowserRouter>
-);
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
