60e7671a3ec229dfd5d4c46454e1b410f1775879
Bouchdak <info@bluelineccs.nl>
2025-09-13 23:26:53 +0200

    UI: ledenbeheer icon (collapsed) + hide dock on /members; routes for /members & /accept-invite

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,4 @@
+// === main.jsx (alleen noodzakelijke aanpassingen toegepast) ===
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { AuthProvider } from './providers/AuthProvider';
@@ -5,6 +6,8 @@ import BluelineChatpilot from './components/BluelineChatpilot';
 import Login from './pages/Login';
 import AuthCallback from './pages/AuthCallback';
 import AcceptInvite from './pages/AcceptInvite';
+import MembersAdmin from './components/MembersAdmin.jsx'; // of jouw pad
+import Protected from './components/Protected.jsx'; // [NIEUW] nodig voor /members
 
 import './index.css';
 
@@ -14,17 +17,26 @@ createRoot(document.getElementById('root')).render(
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/auth/callback" element={<AuthCallback />} />
+        <Route path="/accept-invite" element={<AcceptInvite />} /> {/* [NIEUW] */}
+
         {/* Chatpilot blijft vrij toegankelijk */}
         <Route path="/app" element={<BluelineChatpilot />} />
+
+        {/* Ledenbeheer alleen voor ingelogde users */}
+        <Route
+          path="/members"
+          element={
+            <Protected>
+              <MembersAdmin />
+            </Protected>
+          }
+        />
+
         <Route path="*" element={<Navigate to="/app" replace />} />
       </Routes>
     </AuthProvider>
   </BrowserRouter>
 );
 
-// ...
-<Routes>
-  {/* ... bestaande routes ... */}
-  <Route path="/accept-invite" element={<AcceptInvite />} />
-  {/* ... */}
-</Routes>
+// Opmerking: eventuele losse/verdwaalde <Routes> blokken onderaan dit bestand zijn verwijderd.
+// Alle routes zitten nu netjes binnen het createRoot().
